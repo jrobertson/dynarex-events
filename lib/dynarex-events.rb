66f75fe1ca5e1c8601f6aa416ff15093f7706e68
James <james@ralina.(none)>
2013-08-04 17:46:15 +0100

    feature: DRb server built-in to allow the entries to be refreshed whle running.

--- lib/dynarex-events.rb
+++ lib/dynarex-events.rb
@@ -5,27 +5,69 @@
 require 'chronic_duration'
 require 'dynarex_cron'
 
-class DynarexEvents  < DynarexCron
+class DynarexEvents < DynarexCron
 
-  def initialize(dynarex_file, sps_address=nil)
+  attr_reader :to_a
 
-    dynarex = Dynarex.new dynarex_file
-    entries = dynarex.to_h
+  def initialize(dynarex_file=nil, options={})
+    
+    opt = {sps_address: nil, drb_server: false}.merge options
+    
+    @cron_events  = []
+    
+    @dynarex_file = dynarex_file
+    load_events()
 
-    @cron_entries = entries.inject([]) do |r,h| 
+    @sps_address = opt[:sps_address]
+    
+    if opt[:drb_server] == true then
+      
+      Thread.new {
+        
+        # start up the DRb service
+        DRb.start_service 'druby://:57500', self
+
+        # wait for the DRb service to finish before exiting
+        DRb.thread.join    
+      }
+    end
+  end    
+
+  def load_events()
+    
+    dynarex = Dynarex.new @dynarex_file
+    @entries = dynarex.to_h
+    @cron_events = self.to_a
+  end  
+  
+  alias refresh load_events
+  
+  def start
+    @running = true
+    puts '[' + Time.now.strftime(DF) + '] DynarexEvents started'
+    
+    while @running == true
+      iterate @cron_events
+      sleep 60 # wait for 60 seconds
+    end
+  end  
+  
+  def to_a()
+    
+    @entries.inject([]) do |r,h| 
 
       h[:cron] = ChronicCron.new(h[:date]) 
       h[:job] = 'pub event: ' + h[:title]
 
       if h[:reminder].length > 0 then
         rmndr = {}
-        rmndr[:cron] = Chronic.parse(h[:date]) - ChronicDuration.parse(h[:reminder])
+        rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - ChronicDuration.parse(h[:reminder])).to_s)
         rmndr[:job] = 'pub event: reminder ' + h[:title]
         r << rmndr
       end
 
       r << h
     end
-    @sps_address = sps_address
+    
   end
 end
