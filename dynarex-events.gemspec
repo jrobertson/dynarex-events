eae95929185cc46b648c538e216a5489efb08dfa
James <james@ralina.(none)>
2013-10-26 22:22:44 +0100

    feature: a dynarex events file is now optional

--- dynarex-events.gemspec
+++ dynarex-events.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'dynarex-events'
-  s.version = '0.1.3'
+  s.version = '0.1.5'
   s.summary = 'dynarex-events'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
--- lib/dynarex-events.rb
+++ lib/dynarex-events.rb
@@ -9,23 +9,25 @@ class DynarexEvents < DynarexCron
 
   attr_reader :to_a
 
+  # options: e.g. sps_address, 'sps', drb_server: 58000
+  #
   def initialize(dynarex_file=nil, options={})
     
-    opt = {sps_address: nil, drb_server: false}.merge options
+    opt = {sps_address: nil, drb_server: nil}.merge options
     
-    @cron_events  = []
-    
+    @entries, @cron_events  = [], []
+
     @dynarex_file = dynarex_file
     load_events()
 
     @sps_address = opt[:sps_address]
     
-    if opt[:drb_server] == true then
-      
+    if opt[:drb_server] then
+
       Thread.new {
         
         # start up the DRb service
-        DRb.start_service 'druby://:57500', self
+        DRb.start_service 'druby://:' + opt[:drb_server], self
 
         # wait for the DRb service to finish before exiting
         DRb.thread.join    
@@ -33,11 +35,18 @@ class DynarexEvents < DynarexCron
     end
   end    
 
+  def add_entry(h)
+    # if the entry already exists delete it
+    @entries.delete @entries.find {|x| x[:job] == h[:job]}
+    @entries << h
+  end
+
   def load_events()
     
     dynarex = Dynarex.new @dynarex_file
-    @entries = dynarex.to_h
-    @cron_events = self.to_a
+    @entries = dynarex.to_h || @entries
+    @cron_events = self.to_a if @entries
+    'events loaded and refreshed'
   end  
   
   alias refresh load_events
@@ -53,13 +62,15 @@ class DynarexEvents < DynarexCron
   end  
   
   def to_a()
-    
+
+    return unless @entries
+
     @entries.inject([]) do |r,h| 
 
-      h[:cron] = ChronicCron.new(h[:date] + ' ' + h[:recurring]) 
-      h[:job] = 'pub event: ' + h[:title]
+      h[:cron] = ChronicCron.new(h[:date] + ' ' + h[:recurring].to_s ) 
+      h[:job] ||= 'pub event: ' + h[:title]
 
-      if h[:reminder].length > 0 then
+      if h[:reminder].to_s.length > 0 then
         rmndr = {}
         rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - ChronicDuration.parse(h[:reminder])).to_s)
         rmndr[:job] = 'pub event: reminder ' + h[:title]
@@ -71,4 +82,3 @@ class DynarexEvents < DynarexCron
     
   end
 end
-
