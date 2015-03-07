a4f6afa489bcc94c423740314813fc27690f4e19
Your Name <you@example.com>
2015-03-07 14:37:00 +0000

    Replaced the eventmachine code with the Run_every gem.

--- dynarex-events.gemspec
+++ dynarex-events.gemspec
@@ -1,14 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'dynarex-events'
-  s.version = '0.1.10'
+  s.version = '0.2.0'
   s.summary = 'dynarex-events'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('dynarex_cron')
-  s.add_dependency('chronic_duration')
+  s.add_runtime_dependency('dynarex_cron', '~> 0.5', '>=0.5.3')
+  s.add_runtime_dependency('chronic_duration', '~> 0.10', '>=0.10.4')
   s.signing_key = '../privatekeys/dynarex-events.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/dynarex-events'
+  s.required_ruby_version = '>= 2.1.2'
 end
--- lib/dynarex-events.rb
+++ lib/dynarex-events.rb
@@ -10,48 +10,25 @@ class DynarexEvents < DynarexCron
 
   attr_reader :to_a
 
-  # options: e.g. sps_address: 'sps', drb_port: 58000
-  #
   def initialize(dynarex_file=nil, options={})
     
-    opt = {sps_address: nil, sps_port: '59000', drb_port: nil}.merge options
-
-    @logger = Logger.new(opt[:log],'weekly') if opt[:log]    
-    @entries, @other_entries, @cron_events  = [], [], []
-
-    @dynarex = dynarex_file
+    opt = {sps_address: nil, time_offset: 0}.merge options
+    
+    @time_offset = opt[:time_offset]
+    @cron_events  = []
+    
+    @dynarex_file = dynarex_file
     load_events()
 
-    @sps_address, @sps_port = opt[:sps_address], opt[:sps_port]
+    @sps_address = opt[:sps_address]
     
-    if opt[:drb_port] then
-
-      Thread.new {
-        
-        # start up the DRb service
-        DRb.start_service 'druby://:' + opt[:drb_port], self
-
-        # wait for the DRb service to finish before exiting
-        DRb.thread.join    
-      }
-    end
   end    
-
-  def add_entry(h)
-    # if the entry already exists delete it
-    @other_entries.delete @other_entries.find {|x| x[:job] == h[:job]}
-    @other_entries << h
-    self.refresh
-    'updated and refreshed'
-  end
 
   def load_events()
     
-    dynarex = @dynarex.is_a?(Dynarex) ? @dynarex : Dynarex.new(@dynarex)
-    @entries = (dynarex.to_h || []) + @other_entries
-
-    @cron_events = self.to_a if @entries
-    'events loaded and refreshed'
+    dynarex = Dynarex.new @dynarex_file
+    @entries = dynarex.to_h
+    @cron_events = self.to_a
   end  
   
   alias refresh load_events
@@ -62,36 +39,23 @@ class DynarexEvents < DynarexCron
     puts '[' + Time.now.strftime(DF) + '] DynarexEvents started'
     params = {uri: "ws://%s:%s" % [@sps_address, @sps_port]}    
 
-    c = WebSocket::EventMachine::Client
-    @ws = nil
-
-    EventMachine.run do
-
-      @ws = c.connect(params)
-
-      EM.add_periodic_timer(60) do
-        iterate @cron_events
-      end
-
-    end
-  end 
+    RunEvery.new(seconds: 60) { iterate @cron_events }
+    
+  end  
   
   def to_a()
-
-    return unless @entries
-
+    
     @entries.inject([]) do |r,h| 
 
-      s = h[:expression].to_s.length > 0 ? h[:expression] : 
-        h[:date] + ' ' + h[:recurring].to_s 
+      time = Time.now + @time_offset
+      h[:cron] = ChronicCron.new(h[:date], time) 
+      h[:fqm] = 'event: ' + h[:title]
 
-      h[:cron] = ChronicCron.new(s) 
-      h[:job] ||= 'pub event: ' + h[:title]
-
-      if h[:reminder].to_s.length > 0 then
+      if h[:reminder].length > 0 then
         rmndr = {}
-        rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - ChronicDuration.parse(h[:reminder])).to_s)
-        rmndr[:job] = 'pub event: reminder ' + h[:title]
+        rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - 
+                              ChronicDuration.parse(h[:reminder])).to_s, time)
+        rmndr[:fqm] = 'event: reminder ' + h[:title]
         r << rmndr
       end
 
